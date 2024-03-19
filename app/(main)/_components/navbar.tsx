246699da7a07c32cd337dfa8cf049b928f6a2f4f
Jaya <jaya.lohana24@gmail.com>
2024-03-18 20:33:23 -0400

    commit

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -1,28 +1,32 @@
 "use client";
 
-import { Skeleton } from "@/components/ui/skeleton";
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
+import { MenuIcon } from "lucide-react";
+
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useParams } from "@/node_modules/next/navigation";
-import { useQuery } from "convex/react";
-import { MenuIcon } from "lucide-react";
+
+import { Title } from "./title";
 import { Banner } from "./banner";
 import { Menu } from "./menu";
-import { Title } from "./title";
+import { Publish } from "./publish";
 
 interface NavbarProps {
   isCollapsed: boolean;
-  onResetWdith: () => void;
+  onResetWidth: () => void;
 }
 
-export const Navbar = ({ isCollapsed, onResetWdith }: NavbarProps) => {
+export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
   const params = useParams();
+
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId as Id<"documents">,
   });
+
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full justify-between flex items-center">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
         <div className="flex items-center gap-x-2">
           <Menu.Skeleton />
@@ -37,18 +41,18 @@ export const Navbar = ({ isCollapsed, onResetWdith }: NavbarProps) => {
 
   return (
     <>
-      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
         {isCollapsed && (
           <MenuIcon
             role="button"
-            onClick={onResetWdith}
-            className="h-6 w-6 text-muted-foregorund"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
           />
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
           <div className="flex items-center gap-x-2">
-            <Publish />
+            <Publish initialData={document} />
             <Menu documentId={document._id} />
           </div>
         </div>
